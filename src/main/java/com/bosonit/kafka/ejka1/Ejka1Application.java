ba686acd77cf9ccf39723d100a9a7f509990f4e4
Pedro Corpas Cuesta <pedro.corpas@bosonit.com>
2022-04-06 13:59:36 +0200

    Inicialización

---
+++ src/main/java/com/bosonit/kafka/ejka1/Ejka1Application.java
@@ -0,0 +1,13 @@
+package com.bosonit.kafka.ejka1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ejka1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Ejka1Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/bosonit/kafka/ejka1/Ejka1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bosonit.kafka.ejka1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Ejka1ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
